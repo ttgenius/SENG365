659860e706a58742e202513165085a298e13eba8
ttgenius <melodyzyz@gmail.com>
2017-08-03 22:52:30 +1200

    changes to lab2 ex4

--- lab2/lab2_ex4/app.js
+++ lab2/lab2_ex4/app.js
@@ -1,6 +1,6 @@
 /**
- * Created by yzh231 on 28/07/17.
- */
+* Created by yzh231 on 28/07/17.
+*/
 const express = require('express');
 const bodyParser = require('body-parser')
 const data = require('./users.json');
@@ -9,6 +9,8 @@ const app = express();
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 const users = data.users;
 console.log(users[0]);
+
+console.log(users[0].followers);
 
 app.get('/users',function(req,res){
     res.send(users);
@@ -37,11 +39,11 @@ app.post('/users/', urlencodedParser, function(req,res){
 app.put('/users/:id',function(req,res) {
     let id = req.params.id;
     let user_data = req.body;
-
+    //console.log(user_data);
     for (let user of users) {
         if (id == user.id) {
             let uid = users.indexOf(user);
-            users[uid]=user_data
+            users[uid]=user_data;
             break;
         }
     }
@@ -53,26 +55,132 @@ app.delete('/users/:id',function(req,res) {
     for (let user of users) {
         if (id == user.id) {
             let uid = users.indexOf(user);
-            delete users[uid];
+            users.splice(uid, 1);// remove 1 item at index ‘uid’
+
         }
     }
     res.send(users);
 });
 
-app.post('/users/:id'/'follow'/':fid', function(req,res){
+app.post('/users/:id/follow/:fid',function(req,res) {
+    let id = req.params.id;
+    let fid = req.params.fid;
+    for (let user of users){
+        if (fid == user.id){
+            let uid = users.indexOf(user);
+            users[uid].followers.push(parseInt(id));
+        }
+    }
+    res.send(users);
+})
+
+app.post('/users/:id/unfollow/:fid',function(req,res) {
     let id = req.params.id;
     let fid = req.params.fid;
     for (let user of users) {
-        if (fid == user.fid) {
-            let uid = users.indexOf(user)
-            users[uid].followers.push(id);
+        if (fid == user.id) {
+            let uid = users.indexOf(user);
+            users[uid].followers.pop(id);
         }
-
     }
     res.send(users);
 });
 
+app.get('/users/:id/viewfollowers',function(req,res){
+    let id = req.params.id;
+    let followers = "No followers";
+    for (let user of users){
+        if (id == user.id){
+            let uid = users.indexOf(user);
+            followers = users[uid].followers;
+            break;
+        }
+    }
+    res.send(followers);
+});
+
+app.post('/users/:id/posts/create/:content',function(req,res) {
+    let id = req.params.id;
+    let content = req.params.content ;
+    for (let user of users) {
+        if (id == user.id) {
+            let uid = users.indexOf(user);
+            users[uid].posts.push(content);
+        }
+    }
+    res.send(users);
+});
+
+app.get('/users/:id/posts/view',function(req,res) {
+    let id = req.params.id;
+    let posts = "No posts";
+    for (let user of users) {
+        if (id == user.id) {
+            let uid = users.indexOf(user);
+            posts = users[uid].posts;
+        }
+    }
+    res.send(posts);
+});
+
+app.get('/users/:id/posts/view/:postID',function(req,res) {
+    let id = req.params.id;
+    let post_id = req.params.postID;
+    let post = "No content";
+    for (let user of users) {
+        if (id == user.id) {
+            let uid = users.indexOf(user);
+            post = users[uid].posts[post_id];
+        }
+    }
+    res.send(post);
+});
+
+app.put('/users/:id/posts/update/:postID/:content',function(req,res) {
+    let id = req.params.id;
+    let post_id = req.params.postID;
+    let content = req.params.content;
+    for (let user of users) {
+        if (id == user.id) {
+            let uid = users.indexOf(user);
+            users[uid].posts[post_id]= content;
+            break;
+        }
+    }
+    res.send(users);
+});
+
+app.post('/users/:id/post/delete/:postID',function(req,res) {
+    let id = req.params.id;
+    let post_id = req.params.postID;
+    for (let user of users) {
+        if (id == user.id) {
+            let uid = users.indexOf(user);
+            users[uid].posts.pop(post_id);
+        }
+    }
+    res.send(users);
+});
+
+app.get('/users/:id/posts/view/followers',function(req,res) {
+    let id = req.params.id;
+    let posts = {};
+    let followers = "No followers";
+    for (let user of users) {
+        if (id == user.id) {
+            let uid = users.indexOf(user);
+            followers = users[uid].followers;
+            console.log(followers)
+            for (let follower of followers){
+                let fid = users.indexOf(follower);
+                console.log(follower);
+                posts[fid].push(users[fid].posts);
+            }
+        break;
+        }
+    }
+    res.send(posts);
+});
 app.listen(3000,function(){
     console.log("Example app listening on port 3000!");
 });
-
